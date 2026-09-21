bad3106369fa7e6ce5e5e458a1bb574c24cb0f48
Fabricio Karasz <fabricio.karasz@gmail.com>
2026-09-21 19:57:30 -0300

    corrige tamanho de linhas (#7)

--- src/main/java/br/unicamp/var/apuracao2turno/ResultadoApuracao.java
+++ src/main/java/br/unicamp/var/apuracao2turno/ResultadoApuracao.java
@@ -2,7 +2,8 @@ package br.unicamp.var.apuracao2turno;
 
 /**
  * Representa o resultado da apuração de uma votação majoritária:
- * ou um candidato foi eleito por maioria absoluta, ou o processo segue para segundo turno entre os dois candidatos mais votados.
+ * ou um candidato foi eleito por maioria absoluta, ou o processo segue 
+ * para segundo turno entre os dois candidatos mais votados.
  */
 public sealed interface ResultadoApuracao {
 
@@ -15,7 +16,8 @@ public sealed interface ResultadoApuracao {
     }
 
     /**
-     * Resultado em que nenhum candidato atingiu maioria absoluta e o processo segue para segundo turno entre os dois mais votados.
+     * Resultado em que nenhum candidato atingiu maioria absoluta e o 
+     * processo segue para segundo turno entre os dois mais votados.
      *
      * @param primeiroColocado nome do candidato mais votado
      * @param segundoColocado  nome do segundo candidato mais votado
